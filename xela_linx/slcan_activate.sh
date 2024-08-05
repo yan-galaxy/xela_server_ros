998e6e096c563808a153c727065bbab876650bad
yan-galaxy <yanj3785@gmail.com>
2024-08-05 16:04:39 +0800

    slcan脚本添加参数执行

--- xela_linx/slcan_activate.sh
+++ xela_linx/slcan_activate.sh
@@ -4,6 +4,9 @@ if [ "$(id -u)" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
+# 使用用户提供的参数设置USB设备
+USB_DEVICE="$1"
 
-sudo slcand -o -s8 -t hw -S 3000000 /dev/ttyUSB0
+#sudo slcand -o -s8 -t hw -S 3000000 /dev/ttyUSB0
+sudo slcand -o -s8 -t hw -S 3000000 /dev/ttyUSB"$1"
 sudo ifconfig slcan0 up
